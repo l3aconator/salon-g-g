27d575422ce48a38f9d04ae9d623f1d5eaccd1c8
Kyle Bacon <hello@kylebaconcreative.com>
2021-11-14 22:24:29 -0800

    Cart page

--- page.php
+++ page.php
@@ -11,8 +11,13 @@ $post = Timber::get_post( 'Salon\Models\Post' );
 
 $context['post'] = $post;
 
-if ( is_cart() || is_checkout() ) {
+if ( is_checkout() ) {
 	Timber::render( 'woocommerce/checkout.twig', $context );
+	return;
+}
+
+if ( is_cart() ) {
+	Timber::render( 'woocommerce/cart.twig', $context );
 	return;
 }
 
