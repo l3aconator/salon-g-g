0a6a9985f83df8525db00aff7b47ff5017388dce
Kyle Bacon <hello@kylebaconcreative.com>
2022-01-09 20:54:11 -0800

    fix function

--- static/js/components/quote-modal.js
+++ static/js/components/quote-modal.js
@@ -27,9 +27,8 @@ if (quoteModal) {
   document.addEventListener(
     'wpcf7mailsent',
     function() {
-      document
-        .querySelector('.wpcf7-response-output')
-        .innerHTML('Thank you for your request, we will be in touch with a quote shortly.');
+      document.querySelector('.wpcf7-response-output').innerHTML =
+        'Thank you for your request, we will be in touch with a quote shortly.';
     },
     false,
   );
